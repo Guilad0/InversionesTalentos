57d0b7bc9aa6bf36f89f291b6b646ff658ba43f2
DaroV4 <darovenegasv@gmail.com>
2024-10-14 09:03:35 -0400

    CRUD categoria_posts

---
+++ back/routes/categoria_posts.js
@@ -0,0 +1,95 @@
+const connection = require("../database");
+const { Router } = require("express");
+const router = Router();
+
+router.get("/", (req, res) => {
+  const categoria_posts = "SELECT * FROM categoria_posts";
+
+  connection.query(categoria_posts, (err, results) => {
+    if (err) {
+      //console.log(err);
+      res.status(500).send({
+        error: err,
+        message: "Error en la peticion",
+      });
+    } else {
+      //console.log(result);
+      res.status(200).json({
+        data: results,
+        message: "Lista de categorias posts",
+      });
+    }
+  });
+});
+
+router.post("/", (req, res) => {
+  const { nombre } = req.body;
+
+  const post = `INSERT INTO posts(nombre) VALUES ("${nombre}");`;
+
+  connection.query(categoria_post, (err, results) => {
+    if (err) {
+      //console.log(err);
+      res.status(500).send({
+        error: err,
+        message: "Error en la peticion",
+      });
+    } else {
+      //console.log(result);
+      res.status(200).send({
+        message: "Registro exitoso",
+      });
+    }
+  });
+});
+
+router.put("/:categoria_id", (req, res) => {
+  const { nombre } = req.body;
+  const { categoria_id } = req.params;
+  console.log(categoria_id);
+
+  const producto = `UPDATE categoria_posts SET 
+    nombre = '${nombre}',
+  WHERE categoria = ${categoria_id};`;
+
+  connection.query(categoria_post, (err, results) => {
+    if (err) {
+      //console.log(err);
+      res.status(500).send({
+        error: err,
+        message: "Error en la peticion",
+      });
+    } else {
+      //console.log(result);
+      res.status(200).send({
+        message: "Actualizacion exitosa",
+      });
+    }
+  });
+});
+
+router.delete("/:categoria_id", (req, res) => {
+  const { categoria_id } = req.params;
+
+  const post = `DELETE FROM posts WHERE categoria_id = ${categoria_id};`;
+
+  connection.query(post, (err, results) => {
+    if (err) {
+      //console.log(err);
+      res.status(500).send({
+        error: err,
+        message: "Error en la peticion",
+      });
+    } else {
+      //console.log(result);
+      res.status(200).send({
+        message: "Borrado exitoso",
+      });
+    }
+  });
+});
+
+
+
+
+module.exports = router;
