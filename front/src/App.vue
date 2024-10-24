e00a87341ca5c69b5a1b9499ac9976e577838922
airampoSoruco <airamposoruco@gmail.com>
2024-10-23 23:30:06 -0400

    finished form register

--- front/src/App.vue
+++ front/src/App.vue
@@ -3,6 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
 import Footer from './components/Footer.vue'
 import WhatsAppIco from './components/WhatsAppIco.vue';
+import axios from 'axios';
 
 </script>
 
