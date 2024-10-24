f4f18d56f8bbf29eb040190ecf201f788621e552
DaroV4 <darovenegasv@gmail.com>
2024-10-24 19:05:38 -0400

    correccion navbar, carrusel, correccion bootstrap

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,113 +1,133 @@
 <template>
 
-    <nav class="navbar navbar-expand-lg custom-navbar">
+  <nav class="navbar navbar-expand-lg custom-navbar bg-dark-custom">
 
-        <div class="container-fluid mx-3">
+    <div class="container-fluid mx-3">
 
-            <a class="navbar-brand" href="/">
-                <h2>HAMILO INVERSIONES</h2>
-            </a>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse" id="navbarNav">
 
 
-                    <ul class="navbar-nav mx-auto">
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/">Slice Channel</RouterLink>
-            </li>
-  
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/marketplace">Marketplace</RouterLink>
-            </li>
-  
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/">Propósito</RouterLink>
-            </li>
-  
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/">Cómo funciona</RouterLink>
-            </li>
-  
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/posts">Noticias</RouterLink>
-            </li>
-  
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/">Contactos</RouterLink>
-            </li>
-  
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/">FAQs</RouterLink>
-            </li>
-          </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/">Slice Channel</RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/marketplace">Marketplace
+            </RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/view1">Propósito
+            </RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/view2">Cómo funciona
+            </RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/posts">Posts</RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/view3">Contactos
+            </RouterLink>
+          </li>
+
+          <li class="nav-item">
+            <RouterLink exact-active-class="active" class="nav-link underline-dynamic" to="/faq">FAQs</RouterLink>
+          </li>
+        </ul>
 
 
-                <div class="d-flex align-items-center">
 
-                    <select class="form-select-sm me-2" aria-label="Select Language">
-                        <option value="1">Es</option>
-                        <option value="2">En</option>
-                        <option value="3">De</option>
-                        <option value="4">Fr</option>
-                    </select>
 
-                    <button class="btn boton me-2">Registrarse</button>
-                    <button class="btn boton">Iniciar sesión</button>
+      </div>
 
-                </div>
 
-            </div>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        </div>
+      <a class="navbar-brand" href="/">
+        <h2>HI</h2>
+      </a>
+      <RouterLink class="nav-link" to="sign-login"><i class="fa fa-user-circle fs-3"></i></RouterLink>
 
-    </nav>
+
+    </div>
+
+  </nav>
 
 </template>
 
 <script>
 
-</script>
+</script> 
 
 <style scoped>
-
-a:hover {
-    color: #d9c5b2;
-}
-
-.boton {
-    background-color: #34312d;
-    color: #f3f3f4;
-    border: 1px solid #f3f3f4
-}
-
-.boton:hover {
-    background-color:#d9c5b2;
-    color: black;
-    border: 1px solid black;
-}
-
-.custom-navbar {
-    background-color: var(--gray-color);
-}
-
-a {
-    font-size: 1.2rem;
-    color: #f3f3f4;
-}
-
 .active {
-    background-color: #d9c5b2;
-    color: black;
-    border-radius: 10px;
+  color: var(--yellow-orange) !important;
+  border-radius: 10px !important;
+  padding-bottom: 2px !important;
+  z-index: 1 !important;
+}
+.active::after {
+      transform: scaleX(1) !important;
+      transform-origin: left !important;
 }
 
-nav{
-    height: 10vh;
+nav {
+  height: 7vh;
 }
 
+.nav-link {
+  font-size: 1.1rem;
+  color: var(--white-anti-flash-color);
+}
+
+.nav-link:hover {
+  color: var(--yellow-orange) !important;
+}
+
+i {
+  color: var(--white-anti-flash-color);
+}
+
+i:hover {
+  color: var(--yellow-orange) !important;
+}
+
+.bg-dark-custom {
+  background-color: var(--smoky-dark-color) !important;
+}
+
+.underline-dynamic {
+  display: inline-block;
+  position: relative;
+  padding-bottom: 2px;
+  color: var(--white-anti-flash-color);
+  text-decoration: none;
+}
+
+.underline-dynamic::after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: var(--yellow-orange);
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 0.6s;
+}
+
+.underline-dynamic:hover::after {
+  transform: scaleX(1);
+  transform-origin: left;
+}
 </style>
