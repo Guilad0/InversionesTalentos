21646b47633cb5253e2b518dfc98f84fcb5605e7
DaroV4 <darovenegasv@gmail.com>
2024-10-30 18:55:34 -0400

    paginacion y buscador en solicitudes

--- back/routes/solicitudes_retiro.js
+++ back/routes/solicitudes_retiro.js
@@ -5,31 +5,65 @@ var router = express.Router();
 
 var connection = require("../database");
 
-router.get("/", function (req, res, next) {
 
-  var query = `SELECT solicitudes_retiro.*,concat(usuarios.nombre, ' ', usuarios.apellido , ' - ', usuarios.rol) as username , usuarios.usuario_id, inversiones.inversion_id
-                FROM solicitudes_retiro
-                INNER JOIN usuarios ON solicitudes_retiro.usuario_id = usuarios.usuario_id
-                INNER JOIN inversiones ON solicitudes_retiro.inversion_id = inversiones.inversion_id;`;
-  connection.query(query, function (error, results, fields) {
-    if (error) {
-      console.log(error);
-      res.status(500).send({
-        error: error,
-        message: "Error al realizar la petición",
+router.get("/", function (req, res, next) {
+  var numFilas;
+  var porPagina = 10;
+  var pagina = parseInt(req.query.page, 10) || 1;
+  var busqueda = req.query.search || ''; 
+  var numPaginas;
+  var salto = (pagina - 1) * porPagina;
+  var limite = salto + ',' + porPagina;
+
+  var queryFilas = `SELECT COUNT(*) AS numFilas
+              FROM solicitudes_retiro
+              INNER JOIN usuarios ON solicitudes_retiro.usuario_id = usuarios.usuario_id
+              INNER JOIN inversiones ON solicitudes_retiro.inversion_id = inversiones.inversion_id
+              WHERE concat(usuarios.nombre, ' ', usuarios.apellido) LIKE '%${busqueda}%';`; 
+
+  connection.query(queryFilas, function (error, results, fields) {
+      numFilas = results[0].numFilas;
+      numPaginas = Math.ceil(numFilas / porPagina);
+      var query = `SELECT solicitudes_retiro.*, concat(usuarios.nombre, ' ', usuarios.apellido , ' - ', usuarios.rol) as username, usuarios.usuario_id, inversiones.inversion_id
+                   FROM solicitudes_retiro
+                   INNER JOIN usuarios ON solicitudes_retiro.usuario_id = usuarios.usuario_id
+                   INNER JOIN inversiones ON solicitudes_retiro.inversion_id = inversiones.inversion_id
+                   WHERE concat(usuarios.nombre, ' ', usuarios.apellido) LIKE '%${busqueda}%'
+                   LIMIT ${limite};`; 
+
+      connection.query(query, function (error, results, fields) {
+          if (error) {
+              console.log(error);
+              res.status(500).send({
+                  error: error,
+                  message: "Error al realizar la petición",
+              });
+          } else {
+              console.log(results);
+              var paginas = Array.from({ length: numPaginas }, (_, i) => i + 1);
+
+              var paginacion = {
+                  total: numFilas,
+                  current: pagina,
+                  pages: paginas,
+                  perPage: porPagina,
+                  previous: (pagina > 1) ? (pagina - 1) : null,
+                  next: (pagina < numPaginas) ? (pagina + 1) : null,
+              };
+              res.status(200).send({
+                  pagination: paginacion,
+                  data: results,
+                  message: 'Listando clientes',
+              });
+          }
       });
-    } else {
-      res.status(200).send({
-        data: results,
-        message: "Inversiones consultadas correctamente",
-      });
-    }
   });
 });
 
+
 router.post("/", function (req, res, next) {
-  const {tipo, usuario_id, monto_solicitud, comision_aplicar, monto_recibir, fecha_solicitud, 
-    fecha_aprobacion, foto_identificacion, selfie_identificacion, estado, inversion_id} = req.body;
+  const { tipo, usuario_id, monto_solicitud, comision_aplicar, monto_recibir, fecha_solicitud,
+    fecha_aprobacion, foto_identificacion, selfie_identificacion, estado, inversion_id } = req.body;
 
   var query = ` INSERT INTO solicitudes_retiro, tipo, usuario_id, monto_solicitud,
                 comision_aplicar, monto_recibir, fecha_aprobacion, estado, inversion_id
@@ -54,8 +88,8 @@ router.post("/", function (req, res, next) {
 });
 
 router.put("/:id", function (req, res, next) {
-  const {tipo, usuario_id, monto_solicitud, comision_aplicar, monto_recibir, fecha_solicitud, 
-    fecha_aprobacion, foto_identificacion, selfie_identificacion, estado, inversion_id} = req.body;
+  const { tipo, usuario_id, monto_solicitud, comision_aplicar, monto_recibir, fecha_solicitud,
+    fecha_aprobacion, foto_identificacion, selfie_identificacion, estado, inversion_id } = req.body;
 
   var query = `UPDATE solicitudes_retiro SET 
   tipo = '${tipo}', usuario_id = '${usuario_id}',
@@ -129,7 +163,7 @@ router.patch("/pendiente/:id", function (req, res, next) { //pendiente la solici
   var query = `UPDATE solicitudes_retiro SET
   estado = 'Pendiente'
   WHERE retiro_id = '${req.params.id}';`;
- 
+
   connection.query(query, function (error, results, fields) {
     if (error) {
       console.log(error);
--- front/src/components/WalletRequests.vue
+++ front/src/components/WalletRequests.vue
@@ -3,77 +3,95 @@
         <div class="content">
             <h4 class="d-block mb-2 text-center underline py-5"> Solicitudes de retiro </h4>
             <div class="table-responsive col-md-10 offset-md-1">
-                <table class="table overflow-x-scroll">
-                    <thead>
-                        <tr class="table-secondary">
-                            <th class="custom-size">ID</th>
-                            <th class="custom-size">Nombre usuario - Rol</th>
-                            <th class="custom-size">Monto Solicitud</th>
-                            <th class="custom-size">Comision a aplicar %</th>
-                            <th class="custom-size">Monto a recibir</th>
-                            <th class="custom-size">Fecha de solicitud</th>
-                            <th class="custom-size">Fecha de aprobacion</th>
-                            <th class="custom-size">Estado</th>
-                            <th class="custom-size">Acciones</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="item in solicitudes" :key="item.retiro_id">
-                            <td>{{ item.retiro_id }}</td>
-                            <td>{{ item.username }} </td>
-                            <td>{{ item.monto_solicitud }}</td>
-                            <td>{{ item.comision_aplicar }}</td>
-                            <td>{{ item.monto_recibir }}</td>
-                            <td>{{ new Date(item.fecha_solicitud).toLocaleDateString() }}</td>
-                            <td>{{ new Date(item.fecha_aprobacion).toLocaleDateString() }}</td>
-                            <td>
-                                <span v-if="item.estado == 'Pendiente'" class="badge text-bg-warning">{{ item.estado
-                                    }}</span>
-                                <span v-if="item.estado == 'Aprobado'" class="badge text-bg-success">{{ item.estado
-                                    }}</span>
-                                <span v-if="item.estado == 'Eliminado'" class="badge text-bg-danger">{{ item.estado
-                                    }}</span>
-                            </td>
-                            <td v-if="item.estado == 'Pendiente'">
-                                <button class="btn btn-success btn-sm" @click="aprobado(item.retiro_id)"><i
-                                        class="fa fa-check"></i></button>
-                                <button class="btn btn-danger btn-sm" @click="rechazado(item.retiro_id)"><i
-                                        class="fa fa-times"></i></button>
-                            </td>
-                            <td v-if="item.estado == 'Aprobado'">
-                                <button class="btn btn-warning btn-sm" @click="pendiente(item.retiro_id)"><i
-                                        class="fa fa-clock"></i></button>
-                            </td>
-                            <td v-if="item.estado == 'Eliminado'">
-                                <button class="btn btn-warning btn-sm" @click="pendiente(item.retiro_id)"><i
-                                        class="fa fa-clock"></i></button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div class="col-3 px-5 mb-3">
+                    <input name="search" type="text" v-model="search" class="form-control" placeholder="Buscar ..."
+                        @input="obtenerDatos(1, search)">
+                </div>
+                <div class="table-container">
+                    <table class="table overflow-x-scroll">
+                        <thead>
+                            <tr class="table-secondary">
+                                <th class="custom-size">ID</th>
+                                <th class="custom-size">Nombre usuario - Rol</th>
+                                <th class="custom-size">Monto Solicitud</th>
+                                <th class="custom-size">Comision a aplicar %</th>
+                                <th class="custom-size">Monto a recibir</th>
+                                <th class="custom-size">Fecha de solicitud</th>
+                                <th class="custom-size">Fecha de aprobacion</th>
+                                <th class="custom-size">Estado</th>
+                                <th class="custom-size">Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in solicitudes" :key="item.retiro_id">
+                                <td>{{ item.retiro_id }}</td>
+                                <td>{{ item.username }} </td>
+                                <td>{{ item.monto_solicitud }}</td>
+                                <td>{{ item.comision_aplicar }}</td>
+                                <td>{{ item.monto_recibir }}</td>
+                                <td>{{ new Date(item.fecha_solicitud).toLocaleDateString() }}</td>
+                                <td>{{ new Date(item.fecha_aprobacion).toLocaleDateString() }}</td>
+                                <td>
+                                    <span v-if="item.estado == 'Pendiente'" class="badge text-bg-warning">{{ item.estado
+                                        }}</span>
+                                    <span v-if="item.estado == 'Aprobado'" class="badge text-bg-success">{{ item.estado
+                                        }}</span>
+                                    <span v-if="item.estado == 'Eliminado'" class="badge text-bg-danger">{{ item.estado
+                                        }}</span>
+                                </td>
+                                <td v-if="item.estado == 'Pendiente'">
+                                    <button class="btn btn-success btn-sm" @click="aprobado(item.retiro_id)"><i
+                                            class="fa fa-check"></i></button>
+                                    <button class="btn btn-danger btn-sm" @click="rechazado(item.retiro_id)"><i
+                                            class="fa fa-times"></i></button>
+                                </td>
+                                <td v-if="item.estado == 'Aprobado'">
+                                    <button class="btn btn-warning btn-sm" @click="pendiente(item.retiro_id)"><i
+                                            class="fa fa-clock"></i></button>
+                                </td>
+                                <td v-if="item.estado == 'Eliminado'">
+                                    <button class="btn btn-warning btn-sm" @click="pendiente(item.retiro_id)"><i
+                                            class="fa fa-clock"></i></button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div class="d-flex justify-content-center">
+                    <nav v-if="paginacion.total > 0" aria-label="Page navigation example">
+                        <ul class="pagination">
+                            <li v-if="paginacion.previous == null" class="page-item disabled">
+                                <button class="page-link color-gray fw-bolder rounded-5 border border-3"><i
+                                        class="fa-solid fa-arrow-left"></i></button>
+                            </li>
+                            <li v-else class="page-item">
+                                <button @click="obtenerDatos(paginacion.previous)"
+                                    class="page-link color-gray fw-bolder rounded-5 border border-3"><i
+                                        class="fa-solid fa-arrow-left"></i></button>
+                            </li>
+                            <li v-for="page in paginacion.pages" :key="page" class="page-item"
+                                :class="paginacion.current === page ? 'active' : ''">
+                                <button @click="obtenerDatos(page)"
+                                    class="page-link bg-light mx-2 color-gray fw-bolder rounded-5 border border-3">{{
+                                    page
+                                    }}</button>
+                            </li>
+
+                            <li v-if="paginacion.next == null" class="page-item disabled">
+                                <button class="page-link color-gray fw-bolder rounded-5 border border-3"><i
+                                        class="fa-solid fa-arrow-right"></i></button>
+                            </li>
+                            <li v-else class="page-item ">
+                                <button @click="obtenerDatos(paginacion.next)"
+                                    class="page-link color-gray fw-bolder rounded-5 border border-3"><i
+                                        class="fa-solid fa-arrow-right"></i></button>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
-        <nav v-if="paginacion.total > 0" aria-label="Page navigation example">
-            <ul class="pagination">
-                <li v-if="paginacion.previous == null" class="page-item disabled">
-                    <button class="page-link">Previous</button>
-                </li>
-                <li v-else class="page-item">
-                    <button @click="obtenerDatos(paginacion.previous)" class="page-link">Previous</button>
-                </li>
-                <li v-for="item in paginacion.pages" :key="item" class="page-item"
-                    :class="paginacion.current == item ? 'active' : ''">
-                    <button @click="obtenerDatos(item)" class="page-link">{{ item }}</button>
-                </li>
-
-                <li v-if="paginacion.next == null" class="page-item disabled">
-                    <button class="page-link">Next</button>
-                </li>
-                <li v-else class="page-item ">
-                    <button @click="obtenerDatos(paginacion.next)" class="page-link">Next</button>
-                </li>
-            </ul>
-        </nav>
     </main>
 </template>
 <script setup>
@@ -90,19 +108,17 @@ onMounted(() => {
     obtenerDatos();
 });
 
-const obtenerDatos = async (param = 'p0') => {
+const obtenerDatos = async (page = 1, search = '') => {
     try {
-        var url = BaseURL;
+        var url = `${BaseURL}?page=${page}&search=${search}`;
         const { data } = await axios.get(url);
-        // const response = await axios.get(BaseURL);
-        console.log(data);
         solicitudes.value = data.data;
-
+        paginacion.value = data.pagination;
     } catch (error) {
         console.log(error);
-
     }
 };
+
 
 const aprobado = async (retiro_id) => {
 
@@ -174,11 +190,36 @@ td {
     background-color: rgba(136, 136, 136, 0.76) !important;
 }
 
+.table-container {
+    max-height: 60vh;
+    overflow-y: auto;
+    margin-bottom: 1rem;
+   
+}
+
+.table {
+    width: 100%;
+}
+
+.pagination {
+    margin-top: 1rem;
+    /* Separación superior para los botones */
+}
+
 .pagination .page-item {
     display: inline-block;
 }
- 
+
 .pagination .page-item:nth-child(n+4):nth-last-child(n+4):not(.active) {
     display: none;
+}
+
+.pagination .page-item.active .page-link {
+    background-color: #e0e4ff;
+    color: #080808;
+    font-weight: bold;
+    border: 1.5px solid #b0b8ff;
+    box-shadow: 0px 0px 6px rgb(3, 3, 3);
+    transform: scale(1.05);
 }
 </style>
