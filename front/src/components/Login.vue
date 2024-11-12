33b9ac510495eb2b425d07966fb5aa0aa287dda6
GuiladVillarroel <guilad1233@gmail.com>
2024-11-12 13:02:44 -0400

    alerts

--- front/src/components/Login.vue
+++ front/src/components/Login.vue
@@ -98,6 +98,7 @@ const ingresar = async () => {
       position: 'topRight',
       theme: 'dark',
       color: '#f00', 
+
     })
 
 
@@ -135,6 +136,7 @@ const ingresar = async () => {
       theme: 'dark',
       color: '#5ce65c',
       closeOnEscape: true,
+      progressBarColor: '#FFFFFF'
           })
     setTimeout(() => {
       route.push({ path: "/" });
@@ -162,6 +164,8 @@ const ingresar = async () => {
       position: 'topRight',
       theme: 'dark',
       color: '#f00',
+      closeOnEscape: true,
+      progressBarColor: '#FFFFFF'
     })
 
   }
