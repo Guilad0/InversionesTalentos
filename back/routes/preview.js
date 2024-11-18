99327c6435e263a13b5dde617f2641c399881c7c
DaroV4 <darovenegasv@gmail.com>
2024-11-18 12:49:41 -0400

    funcionalidad de los 4 iconos en las tarjetas de los clientes

--- back/routes/preview.js
+++ back/routes/preview.js
@@ -4,7 +4,18 @@ var router = express.Router();
 
 router.get('/:id', function (req, res, next) {
     const clienteId = req.params.id;
-    const preview = `SELECT e.cargo, l.descripcion, AVG(c.calificacion) AS promedio_calificacion FROM experiencia e INNER JOIN usuarios u ON e.cliente_id = u.usuario_id INNER JOIN logros l ON l.cliente_id = e.cliente_id INNER JOIN comentarios c ON c.cliente_id = u.usuario_id WHERE c.estado = "aprobado" AND u.usuario_id = ?  GROUP BY e.cargo, l.descripcion ORDER BY promedio_calificacion DESC LIMIT 1;`;
+    const preview = `SELECT e.cargo AS experiencia, 
+       l.descripcion AS logro, 
+       AVG(c.calificacion) AS promedio 
+FROM experiencia e 
+INNER JOIN usuarios u ON e.cliente_id = u.usuario_id 
+INNER JOIN logros l ON l.cliente_id = e.cliente_id 
+INNER JOIN comentarios c ON c.cliente_id = u.usuario_id 
+WHERE c.estado = "aprobado" 
+  AND u.usuario_id = '${clienteId}' 
+GROUP BY e.cargo, l.descripcion 
+ORDER BY promedio DESC 
+LIMIT 1;`;
 
     connection.query(preview, [clienteId], function (err, results) {
         if (err) {
